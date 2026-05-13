eeaf37cdc6772a4b074ca61ec13f5b999fb3f7a0
Sadamori Kojaku <skojaku@bingamton.edu>
2026-05-13 05:51:10 -0400

    Fix: add missing Boost include dirs, improve compile script robustness, and update submodule URL

--- scripts/compile_engines.sh
+++ scripts/compile_engines.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
 
-cd engines/bipartiteSBM-MCMC/ && cmake . && make && cd -
-cd engines/bipartiteSBM-KL/ && g++ -O3 -Wall -g -pedantic -o biSBM biSBM.cpp && cd -
+(
+  cd engines/bipartiteSBM-MCMC/ && cmake -DBOOST_ROOT=/tmp/boost_local . && make
+)
+(
+  cd engines/bipartiteSBM-KL/ && g++ -O3 -Wall -g -pedantic -o biSBM biSBM.cpp
+)
